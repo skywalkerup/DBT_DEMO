2e36d928adb62bcbfa536e364c1222f1b9da5ef6
arturartur21212121@gmail.com <arturartur21212121@gmail.com>
2026-03-15 14:22:52 +0000

    daily_weather model created

---
+++ macros/date_utils.sql
@@ -0,0 +1,29 @@
+
+
+{% macro function1(x) %}
+
+CASE WHEN TO_TIMESTAMP({{x}}) < CURRENT_DATE THEN 'PAST'
+ELSE 'FUTURE' END
+
+{% endmacro %}
+
+
+
+{% macro function_station_of_year(x) %}
+
+CASE 
+    WHEN MONTH(TO_TIMESTAMP({{x}})) IN (12,1,2) THEN 'WINTER'
+    WHEN MONTH(TO_TIMESTAMP({{x}})) IN (3,4,5) THEN 'SPRING'
+    WHEN MONTH(TO_TIMESTAMP({{x}})) IN (6,7,8) THEN 'SUMMER'
+    ElSE 'AUTUMN' 
+    END 
+
+{% endmacro %}
+
+
+{% macro get_day_name(x) %}
+CASE 
+    WHEN DAYNAME({{(x)}}) IN ('Sat', 'Sun') THEN 'WEEKEND'
+    ELSE 'BUSINESSDAY'
+    END
+{% endmacro %}
--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -1,5 +1,17 @@
 with CTE as (
-    select STARTED_AT
+
+    select TO_TIMESTAMP(STARTED_AT)  AS STARTED_AT,
+    DATE(TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED_AT,
+    HOUR(TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT,
+     AS DAY_TYPE,
+
+    {{function_station_of_year('STARTED_AT')}} AS STATION_OF_YEAR,
+
+    {{function1('STARTED_AT') }} AS masterpice
+
+
+
+
     from {{ source('demo', 'bike') }}
 )
 
